e7d43a3b17c3f8ee28761c83582f90aca5bf3750
ericjosprice <epriceutexas@gmail.com>
2019-05-01 09:30:44 -0500

    initial commit

---
+++ client/build/precache-manifest.108a731797f652347a0b69c2919f012c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5bd4e5797ed63d6d0c62",
+    "url": "/static/js/main.a621370f.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5bd4e5797ed63d6d0c62",
+    "url": "/static/css/main.b74519fc.chunk.css"
+  },
+  {
+    "revision": "145f2d4092952985cc45a58866b35358",
+    "url": "/index.html"
+  }
+];
